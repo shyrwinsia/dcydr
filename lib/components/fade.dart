20673ebe30698cb84e5578167ffb72e5dc0051e7
Shyrwinsteel Sia <16913966+shyrwinsia@users.noreply.github.com>
2019-11-11 21:07:22 +0800

    Added subtle fade animation

--- lib/components/fade.dart
+++ lib/components/fade.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 
 class FadeIn extends StatefulWidget {
   final Widget child;
+  final double from, to;
+  final int duration;
 
-  FadeIn({@required this.child});
+  FadeIn({@required this.child, from, to, duration})
+      : this.from = from ?? 0.0,
+        this.to = to ?? 1.0,
+        this.duration = duration ?? 200;
 
   @override
   createState() => _FadeInState();
@@ -19,12 +24,12 @@ class _FadeInState extends State<FadeIn> with SingleTickerProviderStateMixin {
 
     _controller = AnimationController(
       vsync: this,
-      duration: Duration(milliseconds: 200),
+      duration: Duration(milliseconds: this.widget.duration),
     );
 
     _animation = Tween(
-      begin: 0.0,
-      end: 1.0,
+      begin: this.widget.from,
+      end: this.widget.to,
     ).animate(_controller);
   }
 
@@ -37,7 +42,62 @@ class _FadeInState extends State<FadeIn> with SingleTickerProviderStateMixin {
   @override
   void didUpdateWidget(FadeIn oldWidget) {
     super.didUpdateWidget(oldWidget);
-    _controller.forward(from: 0.0);
+    _controller.forward(from: this.widget.from);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _controller.forward();
+    return FadeTransition(
+      opacity: _animation,
+      child: widget.child,
+    );
+  }
+}
+
+class FadeOut extends StatefulWidget {
+  final Widget child;
+  final double from, to;
+  final int duration;
+
+  FadeOut({@required this.child, from, to, duration})
+      : this.from = from ?? 1.0,
+        this.to = to ?? 0.0,
+        this.duration = duration ?? 200;
+
+  @override
+  createState() => _FadeOutState();
+}
+
+class _FadeOutState extends State<FadeOut> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: this.widget.duration),
+    );
+
+    _animation = Tween(
+      begin: this.widget.from,
+      end: this.widget.to,
+    ).animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(FadeOut oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _controller.forward(from: this.widget.from);
   }
 
   @override
--- lib/components/switchtile.dart
+++ lib/components/switchtile.dart
@@ -1,3 +1,4 @@
+import 'package:dcydr/components/fade.dart';
 import 'package:dcydr/data/types.dart';
 import 'package:flat_icons_flutter/flat_icons_flutter.dart';
 import 'package:flutter/material.dart';
@@ -15,20 +16,28 @@ class _CustomSwitchTileState extends State<CustomSwitchTile> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(
-        widget._item.name,
-        style: widget._item.selected
-            ? null
-            : TextStyle(
-                color: const Color(0x44000000),
+      title: widget._item.selected
+          ? FadeIn(child: Text(widget._item.name), from: 0.4, to: 1.0)
+          : FadeOut(
+              child: Text(
+                widget._item.name,
               ),
-      ),
+              from: 1.0,
+              to: 0.4,
+            ),
       trailing: widget._item.selected
-          ? Icon(
-              FlatIcons.success,
-              color: const Color(0xff2a86cb),
+          ? FadeIn(
+              child: Icon(
+                FlatIcons.success,
+                color: const Color(0xff2a86cb),
+              ),
             )
-          : null,
+          : FadeOut(
+              child: Icon(
+                FlatIcons.success,
+                color: const Color(0xff2a86cb),
+              ),
+            ),
       onTap: () => {
         setState(() {
           widget._item.selected = !widget._item.selected;
