7b27af6f38c7b024d2a75f6b6c129ac5f2aa52d9
Shyrwinsteel Sia <shyrwinsia@gmail.com>
2019-11-14 16:08:41 +0800

    Removed Firebase in iOS

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,15 +1,13 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 
-@import UIKit;
-@import Firebase;
-
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [FIRApp configure];
-  return YES;
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  // Override point for customization after application launch.
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 @end
