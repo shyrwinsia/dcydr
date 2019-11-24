8cf7b25529f1f52eb1796465bd6646a9f18a27b1
Shyrwinsteel Sia <16913966+shyrwinsia@users.noreply.github.com>
2019-11-24 22:01:05 +0800

    Added bloc to pick page

--- lib/bloc/pickpage/bloc.dart
+++ lib/bloc/pickpage/bloc.dart
@@ -1,3 +1,17 @@
-export 'pick_bloc.dart';
-export 'pick_event.dart';
-export 'pick_state.dart';
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:dcydr/bloc/pickpage/event.dart';
+import 'package:dcydr/bloc/pickpage/state.dart';
+
+class PickBloc extends Bloc<PickEvent, PickState> {
+  @override
+  PickState get initialState => InitialPickState();
+
+  @override
+  Stream<PickState> mapEventToState(
+    PickEvent event,
+  ) async* {
+    // TODO: Add Logic
+  }
+}
