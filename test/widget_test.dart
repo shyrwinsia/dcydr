b9049132c68acb49734b62d7e5c1fb3093935e29
Shyrwinsteel Sia <16913966+shyrwinsia@users.noreply.github.com>
2020-01-05 03:27:02 +0800

    Added unit test for initial state of router

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,23 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:dcydr/bloc/router/bloc.dart';
+import 'package:dcydr/bloc/router/state.dart';
 import 'package:dcydr/main.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  RouterBloc router;
+
+  setUp(() {
+    router = RouterBloc();
+  });
+
+  tearDown(() {
+    router?.close();
+  });
+
+  test('Initial route state is correct', () {
+    expect(router.initialState, RouterHomePage());
+  });
+
   testWidgets('Smoke Test: Check if it loads', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(Dcydr());
