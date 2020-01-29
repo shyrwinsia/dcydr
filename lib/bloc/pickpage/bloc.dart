7a6e54a814305c84f77684f5291f6df873e9aafe
Shyrwinsteel Sia <shyrwin69@gmail.com>
2020-01-29 19:07:12 +0800

    Moved logic of pick item to another function

--- lib/bloc/pickpage/bloc.dart
+++ lib/bloc/pickpage/bloc.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 import 'package:bloc/bloc.dart';
 import 'package:dcydr/bloc/pickpage/event.dart';
 import 'package:dcydr/bloc/pickpage/state.dart';
+import 'package:dcydr/data/types.dart';
 import 'package:dcydr/logger/logger.dart';
 
 class PickPageBloc extends Bloc<PickPageEvent, PickPageState> {
@@ -16,16 +17,7 @@ class PickPageBloc extends Bloc<PickPageEvent, PickPageState> {
   @override
   Stream<PickPageState> mapEventToState(PickPageEvent event) async* {
     if (event is PickItem) {
-      // filter
-      List list = event.list.items.where((e) => e.selected).toList();
-      if (list.length == 0)
-        yield NoItemToPickState();
-      else if (list.length == 1)
-        yield CannotPickState();
-      else {
-        this._pickedItem = _pickItem(list);
-        yield PickedItemState(pick: this._pickedItem);
-      }
+      yield* _pickItem(event.list);
     } else if (event is Reinitialize) {
       yield Uninitialized();
     } else {
@@ -33,12 +25,21 @@ class PickPageBloc extends Bloc<PickPageEvent, PickPageState> {
     }
   }
 
-  String _pickItem(List list) {
-    String pick;
-    // prevent conscutieve
-    do {
-      pick = list.elementAt(rng.nextInt(list.length)).name;
-    } while (pick == this._pickedItem);
-    return pick;
+  Stream<PickPageState> _pickItem(RandomList randomlist) async* {
+    // filter
+    List filteredlist = randomlist.items.where((e) => e.selected).toList();
+    if (filteredlist.length == 0) {
+      yield NoItemToPickState();
+    } else if (filteredlist.length == 1) {
+      yield CannotPickState();
+    } else {
+      String pick;
+      // prevent consecutive
+      do {
+        pick = filteredlist.elementAt(rng.nextInt(filteredlist.length)).name;
+      } while (pick == this._pickedItem);
+
+      yield PickedItemState(pick: pick);
+    }
   }
 }
